f3df7753c51bcdf5f6ae256208cfdaa74fb526ce
Rose Grondin <rose.grondin@gmail.com>
2020-02-20 17:45:16 -0500

    play with the front end of the app

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -17,9 +17,15 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 end
