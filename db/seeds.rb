99d6a5fac2d5d201c4e7b66ec304b19cf630ab7a
Rose Grondin <rose.grondin@gmail.com>
2020-02-20 11:44:10 -0500

    create a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+Ingredient.create(name: 'rum')
+Ingredient.create(name: 'vodka')
+Ingredient.create(name: 'cranberry juice')
+Ingredient.create(name: 'triple sec')
+Ingredient.create(name: 'gin')
+Ingredient.create(name: 'olive')
+Ingredient.create(name: 'dry vermouth')
+Ingredient.create(name: 'lemon juice')
+Ingredient.create(name: 'simple syrup')
+Ingredient.create(name: 'olive juice')
