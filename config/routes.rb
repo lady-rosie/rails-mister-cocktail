b15f86622d33c5ef03b2cdd8835d36ac0f109dcf
Rose Grondin <rose.grondin@gmail.com>
2020-03-18 13:09:03 -0400

    create a delete cocktail button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "cocktails#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:create]
   end
